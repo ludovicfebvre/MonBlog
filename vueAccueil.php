e18f4e45e17e2ec8d30aa430e9eb0d39283e7ebc
ludovic.febvre <ludovic.febvre@CP-SIO-W10-PG03.sio.lan>
2017-05-09 15:31:10 +0200

    Mise en oeuvre du billet et de l'id.

--- vueAccueil.php
+++ vueAccueil.php
@@ -4,10 +4,11 @@
 <?php foreach ($billets as $billet): ?>
   <article>
     <header>
-      <h1 class="titreBillet"><?= $billet['titre'] ?></h1>
+      <a href="<?= "billet.php?id=" . $billet['id'] ?>">
+        <h1 class="titreBillet"><?= $billet['titre'] ?></h1>
+      </a>
       <time><?= $billet['date'] ?></time>
     </header>
-    <p><?= $billet['contenu'] ?></p>
   </article>
   <hr />
 <?php endforeach; ?>
